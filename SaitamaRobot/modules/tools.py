8093236e614682d4e86798c7c7f18f908199d630
ʄɽɑɳƙꍟɳʂʈꍟꀤɳ™ ❬❬kiռց ૨ip⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2021-03-20 21:34:14 +0530

    Update tools.py

--- SaitamaRobot/modules/tools.py
+++ SaitamaRobot/modules/tools.py
@@ -28,7 +28,7 @@ __help__ = f"""
 *Available queries:* Country Code/Country Name/Timezone Name
 • 🕐 [Timezones list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
  
-Report bugs at @TG_Botz.
+Report bugs at @TG_BotZ.
 
 """
 
