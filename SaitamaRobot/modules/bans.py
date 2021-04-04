631f9508cdfa59bab411b42a319ecbd9e3b2edb2
ʄɽɑɳƙꍟɳʂʈꍟꀤɳ™ ❬❬kiռց ૨ip⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2021-04-04 22:31:12 +0530

    Update bans.py

--- SaitamaRobot/modules/bans.py
+++ SaitamaRobot/modules/bans.py
@@ -110,11 +110,11 @@ def ban(update: Update, context: CallbackContext) -> str:
 
         # bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         reply = (
-            f"<code></code><b>Another person whacked by a banhammer...!</b>\n"
-            f"<code></code><b>Banned:</b> {mention_html(member.user.id, html.escape(member.user.first_name))}"
+            f"<code></code><r>Another person whacked by a banhammer...!</r>\n"
+            f"<code></code><r>Banned:</r> {mention_html(member.user.id, html.escape(member.user.first_name))}"
         )
         if reason:
-            reply += f"\n<code></code><b>Reason:</b> \n{html.escape(reason)}"
+            reply += f"\n<code></code><r>Reason:</r> \n{html.escape(reason)}"
         bot.sendMessage(chat.id, reply, parse_mode=ParseMode.HTML, quote=False)
         return log
 
